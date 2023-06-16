dd59040f5e903dbe8624348b79324f22d8180b92
Bo1atit0 <shorungbebolatito@gmail.com>
2023-06-15 18:19:11 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Size of
+ * Return: 0
+ */
+int main(void)
+{
+	printf("size of a char: %d byte(s)");
+	printf("size of an int: %d byte(s)");
+	printf("size of a long int: %d byte(s)");
+	printf("size of a long long int: %d byte(s)");
+	printf("size of a float: %d byte(s)");
+	return (0);
+}
+
