d456897d57bee6721f253661a34b6afd614a8e5e
Bo1atit0 <shorungbebolatito@gmail.com>
2023-06-16 14:07:23 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - alphaBETS
+ * Return: 0
+ */
+int main(void)
+{
+	char low;
+	char up;
+
+	for (low = 'a'; low <= 'z'; low++)
+	for (up = 'A'; up <= 'Z'; up++)
+
+		putchar (low);
+		putchar (up);
+		putchar ('\n');
+
+	return (0);
+}
