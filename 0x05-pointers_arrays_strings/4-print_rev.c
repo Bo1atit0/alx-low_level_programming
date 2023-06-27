a02019a9922c5ba36592211273ccdc5b1e751cd9
Bo1atit0 <shorungbebolatito@gmail.com>
2023-06-27 07:02:37 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints strings in reverse
+ * @s: string to be printed in rev
+ *
+ * Return: 0
+ *
+ */
+
+void print_rev(char *s)
+{
+	int l;
+	int n = 0;
+
+	while (s[n] != '\0')
+		n++;
+
+	for (l = n - 1; l >= 0; l++)
+	{
+		_putchar(s[l]);
+
+			}
+		_putchar('\n');
+}
