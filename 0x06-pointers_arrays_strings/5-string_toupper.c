66aa26d15a17f8a940e7964d96cd01185103597f
Bo1atit0 <shorungbebolatito@gmail.com>
2023-06-30 15:37:08 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - lowercase to uppercase
+ *
+ * @str: pointer to string
+ * @ptr: pointer to string
+ *
+ * Return: char
+ */
+
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+	if (*ptr >= 'a' && *ptr <= 'z')
+	{
+	*ptr = *ptr - ('a' - 'A');
+	}
+	ptr++;
+	}
+	return (str);
+}
