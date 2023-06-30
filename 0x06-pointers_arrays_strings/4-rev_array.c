9b9ab84ba1924b86162b0774f6003796d4ded70b
Bo1atit0 <shorungbebolatito@gmail.com>
2023-06-30 13:32:22 -0700

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse string
+ *
+ * @a: pointer to string
+ * @n: string length
+ *
+ * Return: reversed string
+  */
+
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int i;
+
+
+
+	int len = n - 1;
+
+	for (i = 0; i <= n / 2; i++)
+	{
+	temp = a[i];
+	a[i] = a[len];
+	a[len--] = temp;
+	}
+}
