5e2a02adb1fb67f011a9151c3768c3328ab505e6
Bo1atit0 <shorungbebolatito@gmail.com>
2023-06-30 18:58:43 -0700

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,10 +15,11 @@ char *leet(char *s)
 	char *p = s;
 	char a[12] = "aAeEoOtTlL";
 	char b[12] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1', '5', '7'};
+	int i;
 
 	while (*p != '\0')
 	{
-	for (int i = 0; i < 12; i++)
+	for (i = 0; i < 12; i++)
 	{
 	if (*p == a[i])
 	{
@@ -30,4 +31,5 @@ char *leet(char *s)
 	}
 	{
 		return (s);
+	}
 }
