3390226452abf8a57da484f2b9666a4180a26246
Bo1atit0 <shorungbebolatito@gmail.com>
2023-06-30 18:43:40 -0700

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * leet -  encodes a string into 1337
+ *
+ * @s: string
+ *
+ *
+ * Return: s
+ */
+
+
+char *leet(char *s)
+{
+	char *p = s;
+	char a[12] = "aAeEoOtTlL";
+	char b[12] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1', '5', '7'};
+
+	while (*p != '\0')
+	{
+	for (int i = 0; i < 12; i++)
+	{
+	if (*p == a[i])
+	{
+	*p = b[i];
+	break;
+	}
+	}
+	p++;
+	}
+	{
+		return (s);
+}
