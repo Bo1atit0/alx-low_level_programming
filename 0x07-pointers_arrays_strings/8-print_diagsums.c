995a0669372f728c3e5a09cc307be6e8a52e874b
Bo1atit0 <shorungbebolatito@gmail.com>
2023-07-04 23:53:07 -0700

    8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -22,7 +22,7 @@ void print_diagsums(int *a, int size)
 
 	{
 	sum1 += a[(size * i) + i];
-	sum2 += a[i * (size * (i + 1)) - (i + 1)];
+	sum2 += a[(size * (i + 1)) - (i + 1)];
 	}
 	{
 	printf("%d, %d\n", sum1, sum2);
