bc1482abeb79772fae8af68d954f2b292b6e1457
Bo1atit0 <shorungbebolatito@gmail.com>
2023-07-11 16:11:21 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,13 +12,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s;
-	int i, j, len1, len2, lent;
-
-	i = 0;
-	j = 0;
-	len1 = 0;
-	len2 = 0;
-	lent = 0;
+	int i = 0, j = 0, len1 = 0, len2 = 0, lent = 0;
 
 	if (s1 != NULL)
 	{
@@ -30,20 +24,16 @@ char *str_concat(char *s1, char *s2)
 	while (s2[len2])
 	len2++;
 	}
-
 	lent = len1 + len2;
 	s = (char *)malloc((lent + 1) * sizeof(char));
-
 	if (s == NULL)
 	return (NULL);
-
 	while (i < len1)
 	{
 	s[j] = s1[i];
 	i++;
 	j++;
 	}
-
 	i = 0;
 	while (i < len2)
 	{
