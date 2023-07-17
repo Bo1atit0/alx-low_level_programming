5b4b36705f00cf9d6fb34e09dd24285e38d7ec56
Bo1atit0 <shorungbebolatito@gmail.com>
2023-07-17 14:18:16 -0700

    2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 
-int main()
+/**
+ * main - prints name of file it was compiled from
+ *
+ * Return: name of file
+ */
+
+int main(void)
 {
-	printf("%s\n" ,__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
