1d1461397ac78a6029c7e936a2a98edced7f5431
Bo1atit0 <shorungbebolatito@gmail.com>
2023-07-28 16:04:28 -0700

    assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,26 +1,23 @@
+extern printf   ; Declare the 'printf' function, which is provided externally
+
 section .data
-msg db "Hello, Holberton", 0
-fmt db '%s', 0
+msg: db 'Hello, Holberton', 10
+fmt: db '%s', 0
 
 section .text
-global_main:
 
-extern printf
+global main    ; Declare 'main' as the entry point of the program
 
-main:
+main:          ; The start of the 'main' function
+    push rbp       ; Save the current value of the base pointer to set up a stack frame
+    mov rdi, fmt   ; Move the address of 'fmt' (format string) into the 'rdi' register
+    mov rsi, msg   ; Move the address of 'msg' (message string) into the 'rsi' register
+    xor rax, rax   ; Clear 'rax' register (used to indicate successful program execution)
 
-;push arguments for printf function
+    call printf    ; Call the 'printf' function to print the formatted string
 
-push rdi	;Argument for format string (%s)
-push msg	;Argument for string
+    pop rbp        ; Restore the base pointer, cleaning up the stack frame
 
-	;call printf function
-call printf
-
-;clean up stack
-add rsp, 16
-
-;Exit program
-mov rax, 60	;Syscall number for exit
-xor edi, edi	;exit status 0
-syscall
+    mov rax, 60    ; Set 'rax' to 60 (syscall number for 'exit')
+    xor edi, edi   ; Clear 'edi' register (used for the exit status, 0 in this case)
+    syscall        ; Perform the syscall to terminate the program
