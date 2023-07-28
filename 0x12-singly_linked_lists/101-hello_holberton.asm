dc98ec143b7e62d488b53b1b3ed095c755fba986
Bo1atit0 <shorungbebolatito@gmail.com>
2023-07-28 16:14:50 -0700

    assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -12,12 +12,11 @@ main:          ; The start of the 'main' function
     push rbp       ; Save the current value of the base pointer to set up a stack frame
     mov rdi, fmt   ; Move the address of 'fmt' (format string) into the 'rdi' register
     mov rsi, msg   ; Move the address of 'msg' (message string) into the 'rsi' register
-    xor rax, rax   ; Clear 'rax' register (used to indicate successful program execution)
+    mov rax, 0   ; Clear 'rax' register (used to indicate successful program execution)
 
     call printf    ; Call the 'printf' function to print the formatted string
 
     pop rbp        ; Restore the base pointer, cleaning up the stack frame
 
-    mov rax, 60    ; Set 'rax' to 60 (syscall number for 'exit')
-    xor edi, edi   ; Clear 'edi' register (used for the exit status, 0 in this case)
-    syscall        ; Perform the syscall to terminate the program
+    mov rax, 0    ; Set 'rax' to 0
+	ret
