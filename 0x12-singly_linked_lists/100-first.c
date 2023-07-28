49108f966c3219ba75cf39dbbcfd794d7c575b23
Bo1atit0 <shorungbebolatito@gmail.com>
2023-07-28 11:05:22 -0700

    constructor attribute

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * before_main - constructor function that automatically executes before main
+ *
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+
+}
