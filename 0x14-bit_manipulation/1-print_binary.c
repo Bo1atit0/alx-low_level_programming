836223f37615c71b9dc19f5440a9de0236715df6
bo1atit0 <shorungbebolatito@gmail.com>
2023-09-28 20:20:31 +0100

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,30 +10,28 @@
  */
 
 void print_binary(unsigned long int n)
+{ 
+int i, leadingzero = 1;
+unsigned long int mask;
+  
+if (n == 0)
 {
-  
-  int i, leadingzero = 1;
-  unsigned long int mask;
-  
-  if (n == 0)
-  {
-  _putchar ('0');
-  }
-  
-  for (i = sizeof(n) * 8 - 1; i >= 0; i--)
-  {
-     /* n = n >> 1;*/
-  mask = 1 << i;
-      
-  if (n & mask)
-  {
-  _putchar ('1');
-  leadingzero = 0;
-  }
-  else if (!leadingzero)
+_putchar ('0');
+}  
+for (i = sizeof(n) * 8 - 1; i >= 0; i--)
 {
-  _putchar ('0');
-  }
+ /* n = n >> 1;*/
+mask = 1 << i;
+     
+if (n & mask)
+{
+_putchar ('1');
+leadingzero = 0;
+}
+else if (!leadingzero)
+{
+_putchar ('0');
+}
       
   }
   }
