31c10dafc14058bbc02d047043d0ab67c2f8f446
bo1atit0 <shorungbebolatito@gmail.com>
2023-10-03 11:48:18 +0100

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *create_file - a program that creates a file
+ *@text_content: a NULL terminated string to write to the file
+ *@filename: name of file
+ *Return: 1 if successful, -1 if fails
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int op, wr, i;
+if (filename == NULL)
+{
+return (-1);
+}
+
+op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (op == -1)
+{
+return (-1);
+}
+for (i = 0; text_content[i] != EOF; i++)
+{
+wr = write(op, text_content, i);
+}
+if (wr == -1)
+{
+return (-1);
+}
+close (op);
+return (1);
+}
