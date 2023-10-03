984581af74b17bc8357ab0d8f91dbea6145a37fc
bo1atit0 <shorungbebolatito@gmail.com>
2023-10-03 18:29:16 +0100

    cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+int source, dest;
+ssize_t r, wr;
+int source_close, dest_close;
+char buffer[1024];
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+exit (98);
+}
+
+source = open(argv[1], O_RDONLY);
+r = read(source, buffer, 1024);
+if (source == -1 || r == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit (98);
+}
+
+dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+wr = write(dest, buffer, r);
+if (dest == -1 || wr == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit (98);
+}
+
+source_close = close(source);
+if (source_close == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d", source);
+}
+dest_close = close(dest);
+if (dest_close == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d", dest);
+}
+return (0);
+}
