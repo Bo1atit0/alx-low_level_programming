b7835cc17104853ed25639f777fb91fd5ed9cc29
bo1atit0 <shorungbebolatito@gmail.com>
2023-10-03 12:25:50 +0100

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,15 +9,15 @@
 
 int create_file(const char *filename, char *text_content)
 {
-int op, wr, i;
+int fd, wr, i;
 
 if (filename == NULL)
 {
 return (-1);
 }
 
-op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (op == -1)
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (fd == -1)
 {
 return (-1);
 }
@@ -26,14 +26,14 @@ if (text_content == NULL)
 {
 text_content = "";
 }
-for (i = 0; text_content[i] != '\0'; i++)
+for (i = 0; text_content[i]; i++)
 {
-wr = write(op, text_content, i);
+wr = write(fd, text_content, i);
 }
 if (wr == -1)
 {
 return (-1);
 }
-close (op);
+close (fd);
 return (1);
 }
