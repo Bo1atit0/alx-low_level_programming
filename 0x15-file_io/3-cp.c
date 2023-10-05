a572e26e3f202f6a41abbc7c3bb47e2ca46d124f
bo1atit0 <shorungbebolatito@gmail.com>
2023-10-05 20:45:09 +0100

    corrections

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,8 @@
 int main(int argc, char *argv[])
 {
 int source, dest;
-ssize_t r, wr;
+ssize_t r;
+ssize_t wr;
 int source_close, dest_close;
 char buffer[1024];
 
@@ -22,30 +23,43 @@ exit (98);
 }
 
 source = open(argv[1], O_RDONLY);
-r = read(source, buffer, 1024);
-if (source == -1 || r == -1)
+if (source == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit (98);
 }
-
 dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (dest == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+exit (98);
+}
+while ((r = read(source, buffer, 1024)) > 0)
+{
 wr = write(dest, buffer, r);
-if (dest == -1 || wr == -1)
+if (wr == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+exit (98);
+}
+if (r == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit (98);
 }
-
+}
 source_close = close(source);
 if (source_close == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d", source);
+exit (100);
 }
 dest_close = close(dest);
 if (dest_close == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d", dest);
+exit (100);
 }
 return (0);
 }
+
