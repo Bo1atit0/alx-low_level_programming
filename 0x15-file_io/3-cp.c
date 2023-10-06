59c003b48091bc83739fe55ef16fa31d53b799da
bo1atit0 <shorungbebolatito@gmail.com>
2023-10-06 07:52:41 +0100

    file io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,57 +8,54 @@
 *Return: nothing
 */
 
+int handle_errors(int source, int dest, char **argv)
+{
+if (source == -1)
+{
+    dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+if (dest == -1)
+{
+    dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+exit(98);
+}
+}
+
 int main(int argc, char *argv[])
 {
 int source, dest;
-ssize_t r;
-ssize_t wr;
+ssize_t r, wr;
 int source_close, dest_close;
 char buffer[1024];
 
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
-exit (98);
+exit(98);
 }
-
 source = open(argv[1], O_RDONLY);
-if (source == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit (98);
-}
+handle_errors(source, 0, argv[1]);
 dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (dest == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
-exit (98);
-}
+handle_errors(0, dest, argv[2]);
+
 while ((r = read(source, buffer, 1024)) > 0)
 {
 wr = write(dest, buffer, r);
-if (wr == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
-exit (98);
-}
-if (r == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit (98);
-}
+handle_errors(0, wr, argv[2]);
+handle_errors(r, 0, argv[1]);
 }
 source_close = close(source);
 if (source_close == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d", source);
-exit (100);
+exit(100);
 }
 dest_close = close(dest);
 if (dest_close == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d", dest);
-exit (100);
+exit(100);
 }
 return (0);
 }
