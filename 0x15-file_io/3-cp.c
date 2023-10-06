1dd1da48a59442c639b1c9e9214883a5c1a3fc74
bo1atit0 <shorungbebolatito@gmail.com>
2023-10-06 00:15:01 -0700

    corrections

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,25 +2,33 @@
 #include <stdio.h>
 
 /**
+ * handle_errors - for error messages in code
+ * @source: source
+ * @dest: destination
+ * @argv: argument vector
+ * Return: nothing
+ */
+
+void handle_errors(int source, int dest, char *argv[])
+{
+if (source == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+if (dest == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+exit(98);
+}
+}
+
+/**
 *main - a program that checks code
 *@argv: argument vector
 *@argc: argument count
 *Return: nothing
 */
-
-int handle_errors(int source, int dest, char **argv)
-{
-if (source == -1)
-{
-    dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-if (dest == -1)
-{
-    dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
-exit(98);
-}
-}
 
 int main(int argc, char *argv[])
 {
@@ -35,15 +43,15 @@ dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
 exit(98);
 }
 source = open(argv[1], O_RDONLY);
-handle_errors(source, 0, argv[1]);
+handle_errors(source, 0, argv);
 dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-handle_errors(0, dest, argv[2]);
+handle_errors(0, dest, argv);
 
 while ((r = read(source, buffer, 1024)) > 0)
 {
 wr = write(dest, buffer, r);
-handle_errors(0, wr, argv[2]);
-handle_errors(r, 0, argv[1]);
+handle_errors(0, wr, argv);
+handle_errors(r, 0, argv);
 }
 source_close = close(source);
 if (source_close == -1)
