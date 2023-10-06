705581bd303ea1e0f6b443343aba125721c5610d
bo1atit0 <shorungbebolatito@gmail.com>
2023-10-06 21:31:24 +0100

    file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,16 +11,16 @@
 
 void handle_errors(int source, int dest, char *argv[])
 {
-	if (source == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (dest == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
-		exit(99);
-	}
+if (source == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+if (dest == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+exit(99);
+}
 }
 
 /**
@@ -40,23 +40,25 @@ char buffer[1024];
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
-exit(98);
+exit(97);
 }
 source = open(argv[1], O_RDONLY);
 handle_errors(source, 0, argv);
 dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-handle_errors(0, dest, argv);
+handle_errors(source, dest, argv);
 
 r = 1024;
 while (r == 1024)
 {
 
-r = read(dest, buffer, 1024);
+r = read(source, buffer, 1024);
 if (r == -1)
 handle_errors(-1, 0, argv);
+
 wr = write(dest, buffer, r);
 if (wr == -1)
 handle_errors(0, -1, argv);
+
 }
 source_close = close(source);
 if (source_close == -1)
