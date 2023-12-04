77bf0ac3bd4f41ae64e7012c828a67421b1f87d7
bo1atit0 <shorungbebolatito@gmail.com>
2023-12-04 10:20:25 -0800

    betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *newtable = NULL;
+
 if (size == 0)
 {
 return (NULL);
@@ -19,6 +20,7 @@ if (newtable == NULL)
 {
 return (NULL);
 }
+
 newtable->size = size;
 newtable->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
 if (newtable->array == NULL)
