3a424d361b4af19a6f6ffc9578fc398f7e08a3e3
bo1atit0 <shorungbebolatito@gmail.com>
2023-12-06 23:03:36 -0800

    handle collision

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,8 +4,8 @@
 * hash_table_set - function that adds an element to the hash table
 * @ht: pointer to the hash table
 * @key: key
-* @value: value 
-* Return: 1 if it succeeds 0 if not 
+* @value: value
+* Return: 1 if it succeeds 0 if not
 */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
@@ -29,9 +29,9 @@ new_element->key = strdup(key);
 new_element->value = strdup(value);
 new_element->next = NULL;
 
-if (ht->array[index] == NULL)
+if (ht->array[index] != NULL)
 {
-new_element->next = ht->array[index];
+	new_element->next = ht->array[index];
 }
 ht->array[index] = new_element;
 return (1);
