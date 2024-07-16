8d8b4e4e90266dde3c7210a42c219834230b2845
bolatito <shorungbebolatito@gmail.com>
2024-07-16 14:52:17 +0100

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -18,6 +18,11 @@ int binary_search(int *array, size_t size, int value)
 	l = 0;
 	r = size - 1;
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
 	while (l <= r)
 	{
 		printf("Searching in array: ");
