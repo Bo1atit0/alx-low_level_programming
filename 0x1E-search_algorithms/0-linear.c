2db9512103823c9c241c96f083c991ab875a2abc
bolatito <shorungbebolatito@gmail.com>
2024-07-16 14:44:11 +0100

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -23,7 +23,7 @@ int linear_search(int *array, size_t size, int value)
 	for (i = 0; i < size; i++)
 	{
 		/* Value checked array[0] = [10] */
-		printf("Value Checked array[%lu] = [%d]\n", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			return (i);
